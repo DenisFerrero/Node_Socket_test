71cecb4b6b55d6458750fb18124722ff31bf49ce
Denis Ferrero <ferrero.denis@protonmail.com>
2020-12-19 23:29:34 +0100

    Added bootstrap and POST example

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+// Import libraries
 const express = require("express");
 
 var app = express();
@@ -9,8 +10,24 @@ app.get('/', function (req, res) {
   res.render('pages/index.ejs');
 })
 
-app.get('/hello', function (req, res) {
-  res.render('pages/hello_world.ejs', { name: 'Hello world' });
-})
+// Manage multiple routes using app.route
+app.route('/hello')
+  // Manage get route by returning Hello World as name
+  .get(function (req, res) {
+    res.render('pages/hello_world.ejs', { name: 'Hello world' });
+  })
+
+  // Manage post route by returning the input field as name
+  .post(function (req, res) {
+    /*
+    * By using req.body.name you'll access data sended by the frontend
+    * the .name parameter is the name attribute of the input field.
+    * If you don't want to use form you could use an ajax call
+    * (https://api.jquery.com/Jquery.ajax/)
+    * if you do that the data'll be saved into req.query
+    */
+    console.log(`Save to database: ${req.body.name}`);
+    res.render('pages/hello_world.ejs', { name: req.body.name });
+  })
 
 module.exports = app;
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.set('view engine', 'ejs');
 // Default action to parse data from request
 app.use(bodyparser.urlencoded({extended:false}));
 
+// Serve and access node_modules as localhost:3000/modules (used to use library in frontend)
+app.use("/modules", express.static(path.join(__dirname, 'node_modules')));
+
 // Import the route from ./controller/index.js
 app.use('/', require('./controllers/index.js'));
 
@@ -20,4 +23,8 @@ app.listen(3000, function () {
   console.log('Server running on localhost:3000');
 });
 
+/* 
+* Exporting the variable app
+* when you'll import this file you'll get the app variable content
+*/
 module.exports = app;
